0a8888bc749607f5614fe588bbf86d1f4fcfe461
Jacob Carpenter <github@jacobcarpenter.com>
2022-04-24 09:19:12 -0700

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+/* eslint-env node */
+module.exports = {
+	ignorePatterns: ['src/constants.js'],
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:react/jsx-runtime',
+		'plugin:react-hooks/recommended',
+
+		// must be last? https://github.com/prettier/eslint-plugin-prettier#recommended-configuration
+		'plugin:prettier/recommended',
+	],
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['react', 'prettier'],
+	settings: {
+		react: {
+			version: 'detect',
+		},
+	},
+	rules: {
+		'react/prop-types': ['error', { skipUndeclared: true }],
+		'prettier/prettier': 'error',
+	},
+};
